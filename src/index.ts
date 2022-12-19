cf919661f4b60f3673e823a750275a822487fd7c
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-12-19 00:31:31 -0300

    adicionado as rotas e uma rota extra draft

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,77 @@ const app = express();
 
 app.use(express.json());
 
-// ... your REST API routes will go here
+// busca todos os usuarios
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
+});
+
+// busca os posts publicados
+app.get("/feed", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
+  res.json(posts);
+});
+// busca os posts não publicados
+app.get("/draft", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    where: { published: false },
+    include: { author: true },
+  });
+  res.json(posts);
+});
+
+// busca post por id
+app.get(`/post/:id`, async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+// cria um novo usuario
+app.post(`/user`, async (req, res) => {
+  const result = await prisma.user.create({
+    data: { ...req.body },
+  });
+  res.json(result);
+});
+
+// cria um novo post como rascunho
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
+
+// Define o campo published (publicado) de um post como true (verdadeiro).
+app.put("/post/publish/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.update({
+    where: { id: Number(id) },
+    data: { published: true },
+  });
+  res.json(post);
+});
+
+// deleta um post pot id
+app.delete(`/post/:id`, async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.delete({
+    where: { id: Number(id) },
+  });
+  res.json(post);
 });
 
 app.listen(3000, () =>
